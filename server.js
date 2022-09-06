9c1bd8d9656dc7d5f1d365836c058bced428473b
Ayushi Khurana <52856951+ayushi2110@users.noreply.github.com>
2022-09-06 12:19:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,4 @@ const connectionDB = require('./Config/Connection');
 
 connectionDB();
 
-app.get('/', (req, res) => {
-    res.send('hello')
-})
 app.listen(process.env.PORT, () => console.log(`success ${process.env.PORT}`))
